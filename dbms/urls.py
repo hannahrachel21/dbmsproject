cbdd178d06b2dc55c1b7a13d65afb7e2af3b75bc
Hannah <u2103100@rajagiri.edu.in>
2023-12-18 14:24:32 +0530

    AdminComplete

--- dbms/urls.py
+++ dbms/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import employee_list, delete_employee, update_employee
-from .views import members_list, delete_members, update_members
+from .views import employee_list, delete_employee, update_employee,update_employee_confirm
+from .views import members_list, delete_members, update_members,update_members_confirm
 from .views import adminsucess
 
 urlpatterns = [
@@ -8,7 +8,10 @@ urlpatterns = [
     path('employee_list/', employee_list, name='employee_list'),
     path('delete_employee/', delete_employee, name='delete_employee'),
     path('update_employee/<int:employee_id>/', update_employee, name='update_employee'),
+    path('update_employee/<int:employee_id>/confirm/', update_employee_confirm, name='update_employee_confirm'),
     path('members_list/', members_list, name='members_list'),
     path('delete_members/', delete_members, name='delete_members'),
     path('update_members/<int:members_id>/', update_members, name='update_members'),
-]
+    path('update_members/<int:members_id>/confirm/', update_members_confirm, name='update_members_confirm'),
+    path('adminsucess/', adminsucess, name='adminsucess'),
+    ]
--- dbms/views.py
+++ dbms/views.py
@@ -26,16 +26,27 @@ def delete_employee(request):
         return redirect('employee_list')
 
 def update_employee(request, employee_id):
-    employee = employee.objects.get(id=employee_id)
+    employee_instance = employee.objects.get(id=employee_id)
+    form = employeeForm(instance=employee_instance)
+
     if request.method == 'POST':
-        form = employeeForm(request.POST, instance=employee)
+        form = employeeForm(request.POST, instance=employee_instance)
+        if form.is_valid():
+            form.save()
+            return redirect('update_employee_confirm', employee_id=employee_id)
+
+    return render(request, 'update_employee.html', {'form': form, 'employee_id': employee_id})
+
+def update_employee_confirm(request, employee_id):
+    employee_instance = employee.objects.get(id=employee_id)
+
+    if request.method == 'POST':
+        form = employeeForm(request.POST, instance=employee_instance)
         if form.is_valid():
             form.save()
             return redirect('employee_list')
-    else:
-        form = employeeForm(instance=employee)
 
-    return render(request, 'update_employee.html', {'form': form, 'employee_id': employee_id})
+    return render(request, 'update_employee_confirm.html', {'form': employeeForm(instance=employee_instance)})
 
 #member views
 def members_list(request):
@@ -57,14 +68,29 @@ def delete_members(request):
         return redirect('members_list')
 
 def update_members(request, members_id):
-    memberr = members.objects.get(id=members_id)
+    memberr_instance = members.objects.get(id=members_id)
+    form = memberForm(instance=memberr_instance)
+
     if request.method == 'POST':
-        form = memberForm(request.POST, instance=memberr)
+        form = memberForm(request.POST, instance=memberr_instance)
+        if form.is_valid():
+            form.save()
+            return redirect('update_members_confirm', members_id=members_id)
+
+
+    return render(request, 'update_members.html', {'form': form, 'members_id': members_id})
+
+
+def update_members_confirm(request, members_id):
+    memberr_instance = members.objects.get(id=members_id)
+
+    if request.method == 'POST':
+        form = memberForm(request.POST, instance=memberr_instance)
         if form.is_valid():
             form.save()
             return redirect('members_list')
-    else:
-        form = memberForm(instance=memberr)
 
-    return render(request, 'update_members.html', {'form': form, 'members_id': members_id})
+
+    return render(request, 'update_members_confirm.html', {'form': memberForm(instance=memberr_instance)})
+
 
